1a718d5f9801677fe1e3fe5adf09f1c494309c05
paddywide 7735 <wei.song.work@gmail.com>
2024-12-20 17:55:47 +1100

    chcange project name

--- ExchangeRate.Persistence/DatabaseContext/ExchangeRateDatabaseContext.cs
+++ ExchangeRate.Persistence/DatabaseContext/ExchangeRateDatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,9 @@ namespace ExchangeRate.Persistence.DatabaseContext
 {
     public class ExchangeRateDatabaseContext : DbContext
     {
+        public DbSet<CurrencyCode> CurrencyCode { get; set;  }
         public ExchangeRateDatabaseContext(DbContextOptions<ExchangeRateDatabaseContext> options) : base(options)
         {
-            public DbSet<CurrencyCode> CurrencyCode { get; set;  }
         }
     }
 }
