91448d980bea91b715bdee6e985109f09429c0a3
paddywide 7735 <wei.song.work@gmail.com>
2025-01-20 23:34:13 +1100

    update start_service.sh

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Ensure correct permissions
+sudo chown -R www-data:www-data /wwwroot/exchange-rate
+sudo chmod -R 755 /wwwroot/exchange-rate
+
 echo "Starting .NET Core application..."
 sudo systemctl start exchange-rate.service
 echo "Application started."
