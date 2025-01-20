ac6c89c8428d84fa7a7652c90cb5b9967cdfa562
paddywide 7735 <wei.song.work@gmail.com>
2025-01-20 21:27:12 +1100

    update yml and stop_service.sh

--- stop_service.sh
+++ stop_service.sh
@@ -2,3 +2,8 @@
 echo "Stopping existing .NET Core application..."
 sudo systemctl stop exchange-rate.service || true
 echo "Existing application stopped."
+
+
+chmod +w /wwwroot/exchange-rate/SQLite.dll 2>/dev/null || true
+rm -f /wwwroot/exchange-rate/SQLite.dll 2>/dev/null || true
+
